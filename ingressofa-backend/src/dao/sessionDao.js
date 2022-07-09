8592096f4d4bd8379b4ff332bfbe8c5621b83265
jazielloureiro <jazielloureiro17@gmail.com>
2022-07-09 13:57:10 -0300

    add getSessionById function

--- ingressofa-backend/src/dao/sessionDao.js
+++ ingressofa-backend/src/dao/sessionDao.js
@@ -4,5 +4,10 @@ module.exports = {
     addSession: session => db.query(
         'INSERT INTO "Session" ("Date", "IdLocal", "IdEvent", "Price") VALUES ($1, $2, $3, $4)',
         [session.date, session.local, session.event, session.price]
+    ),
+
+    getSessionById: id => db.oneOrNone(
+        'SELECT "Date", "IdLocal", "IdEvent", "Price" FROM "Session" WHERE "Id" = $1',
+        [id]
     )
 }
