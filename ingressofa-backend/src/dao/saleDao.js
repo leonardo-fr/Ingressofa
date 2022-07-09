89ebc3b5c568e44d67ac8de36febbbf14734d99a
jazielloureiro <jazielloureiro17@gmail.com>
2022-07-09 16:18:24 -0300

    improve addSale dao function

--- ingressofa-backend/src/dao/saleDao.js
+++ ingressofa-backend/src/dao/saleDao.js
@@ -1,8 +1,8 @@
 const db = require('./connection')
 
 module.exports = {
-    addSale: sale => db.query(
-        'INSERT INTO "Sale" ("Protocol", "Value", "Date", "IdUser", "PaymentMethod", "Type") VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4, $5)',
+    addSale: sale => db.one(
+        'INSERT INTO "Sale" ("Protocol", "Value", "Date", "IdUser", "PaymentMethod", "Type") VALUES ($1, $2, CURRENT_TIMESTAMP, $3, $4, $5) RETURNING "Id"',
         ['1', sale.totalValue, sale.user, sale.paymentMethod, sale.type]
     )
 }
