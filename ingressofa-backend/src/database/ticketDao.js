8cc8280c0c90361ecd5be2f81ef58546a3c0f4cb
jazielloureiro <jazielloureiro17@gmail.com>
2022-07-10 08:01:22 -0300

    add getTicketsBySession to ticket dao

--- ingressofa-backend/src/database/ticketDao.js
+++ ingressofa-backend/src/database/ticketDao.js
@@ -4,5 +4,10 @@ module.exports = {
     addTicket: ticket => db.query(
         'INSERT INTO "Ticket" ("Price", "IdSession", "IdSale", "Type", "Seat") VALUES ($1, $2, $3, $4, $5)',
         [ticket.price, ticket.session, ticket.sale, ticket.type, ticket.seat]
+    ),
+
+    getTicketsBySession: session => db.manyOrNone(
+        'SELECT * FROM "Ticket" WHERE "IdSession" = $1',
+        [session]
     )
 }
