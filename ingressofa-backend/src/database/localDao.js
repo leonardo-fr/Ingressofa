49546793a3bb9f62edf29d6350d21507dca058bb
jazielloureiro <jazielloureiro17@gmail.com>
2022-07-10 07:53:11 -0300

    add getLocalById to local dao

--- ingressofa-backend/src/database/localDao.js
+++ ingressofa-backend/src/database/localDao.js
@@ -4,5 +4,10 @@ module.exports = {
     addLocal: local => db.query(
         'INSERT INTO "Local" ("Name", "Capacity") VALUES ($1, $2)',
         [local.name, local.capacity]
+    ),
+
+    getLocalById: id => db.oneOrNone(
+        'SELECT * FROM "Local" WHERE "Id" = $1',
+        [id]
     )
 }
