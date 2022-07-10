5a4939d07dd63c573d972171787890a4e6300543
jazielloureiro <jazielloureiro17@gmail.com>
2022-07-09 21:40:20 -0300

    refactor authController

--- ingressofa-backend/src/controllers/authController.js
+++ ingressofa-backend/src/controllers/authController.js
@@ -1,15 +1,14 @@
 const authService = require('./../services/authService')
 
 module.exports = {
-    login: async (req, res) => {
-        try {
-            const user = await authService.login(req.body)
-
-            req.session.user = user
-
-            res.status(200).send()
-        } catch (error) {
-            res.status(400).send({error})
-        }
+    login: (req, res) => {
+        authService.login(req.body)
+            .then(user => {
+                req.session.user = user
+                res.status(200).send()
+            })
+            .catch(error =>
+                res.status(400).send({ error })
+            )
     }
 }
